023aeab4bc5e5008482bb69f33c8cddf33443b37
AkshataHasabi <akshuh818@gmail.com>
2022-03-01 21:09:35 +0530

    ADDED:welcome message

---
+++ BinarySearchTreeMain.java
@@ -0,0 +1,9 @@
+package com.bridgelab.binarytree;
+
+public class BinarySearchTreeMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Binary Search Tree programms");
+	}
+
+}
